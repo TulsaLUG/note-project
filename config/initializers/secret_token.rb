94dc574a875325bcd37165b9ed7db1e9203174b4
Dennis Hodapp <thehodapp@gmail.com>
2012-10-12 10:22:17 -0500

    Generated RoR framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoteProject::Application.config.secret_token = '2f68cb1ff85fb5f8b3eba3e559f9a982793956ac3353700e645dfc1bf05945d050386503237f1cd31a001030747b80b1746252dd24cfb3230ef8e2806e65fab7'
